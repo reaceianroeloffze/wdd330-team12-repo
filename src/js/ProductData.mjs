cbabe9df0b2262b0490c6637bde266f1d74a7e83
Avee1000 <eddyidahosa01@gmail.com>
2024-11-12 11:47:18 +0100

    changes (my way of solving the problem)

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,32 +1,42 @@
 function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error("Bad Response");
+    }
   }
-}
-
+  
 export default class ProductData {
-  constructor(category) {
-    this.category = category;
-    this.path = `../json/${this.category}.json`;
-  }
-
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
-  }
-
-  async findProductById(ids) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === ids); 
-  }
-
-  async getProductId(ids) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === ids).Id;
-  }
-  //I added this method so that it can return the product Id after find it and then we can use the
-  //same id as a key in the localstorage. This will help because we can then use different keys to store different products.
+    constructor(category) {
+        this.category = category;
+        this.path = `../json/${this.category}.json`;
+    }
+    getData() {
+        return fetch(this.path)
+            .then(convertToJson)
+            .then((data) => data);
+      
+        //ALTERNATIVE WAY
+        // async function getData() {
+        //   const response = await fetch(this.path);
+        //   const data = await response.json();
+        //   return data;
+        // }
+    }
+    async findProductById(ids) {
+        const products = await this.getData();
+        return products.find((item) => item.Id === ids);
+        // item represents each element in the products array as .find() through it.
+        // it could be represented as anything.
+        // item.id === id is a condition that check if the id property of item matches the provided id in the function call.
+    }
+  
+  
+    async getProductId(ids) {
+        const products = await this.getData();
+        return products.find((item) => item.Id === ids).Id;
+    }
+    //I added this method so that it can return the product Id after find it and then we can use the
+    //same id as a key in the localstorage. This will help because we can then use different keys to store different products.
 }
+    
--- src/js/product.js
+++ src/js/product.js
@@ -3,16 +3,29 @@ import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
-function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// function addProductToCart(product) {
+//   setLocalStorage('so-cart', product);
+// }
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+// // add listener to Add to Cart button
+// document
+//   .getElementById('addToCart')
+//   .addEventListener('click', addToCartHandler);
+
+
+const elements = document.querySelectorAll('#addToCart');
+
+elements.forEach((element) => {
+  element.addEventListener('click',async function addToCartHandler(e) {
+    const product = await dataSource.findProductById(e.target.dataset.id);
+    const productId = await dataSource.getProductId(e.target.dataset.id);
+    setLocalStorage(productId, product);
+  });
+})
+
+    
