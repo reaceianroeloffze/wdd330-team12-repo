8bd89226243634e6f0f64872d6b1dd60f9c295cf
tiffster2500 <tiff6377@hotmail.com>
2024-11-13 16:26:12 -0700

    Steps 1-6 of individual activity completed

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,33 @@
+function renderProductCard(product) {
+    return `<li class="product-card">
+        <a href="product_pages/?product=${product.Id}">
+            <img
+                src=${product.Image}
+                alt=${product.NameWithoutBrand}
+            />
+            <h3 class="card__brand">${product.Brand.Name}</h3>
+            <h2 class="card__name">
+                ${product.NameWithoutBrand}
+            </h2>
+            <p class="product-card__price">$${product.FinalPrice}</p></a
+        >
+    </li>`;
+}
+
+export default class ProductListing {
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+
+    async init() {
+        const data = await this.dataSource.getData();
+        data.forEach((product) => {
+            this.listElement.insertAdjacentHTML(
+                'beforeEnd',
+                renderProductCard(product)
+            );
+        });
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import ProductData from './ProductData.mjs';
 import ProductListing from './ProductList.mjs';
 
 const dataSource = new ProductData('tents');
-const listElement = document.getElementsByClassName('.product-list');
+const listElement = document.querySelector('.product-list');
 const products = new ProductListing('tents', dataSource, listElement);
 
 products.init();
