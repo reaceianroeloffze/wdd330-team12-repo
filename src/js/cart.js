63be64a63ae04ac711bc2abb35183904db79dd5d
ssewanyana-nicholas <nicssewanyana@gmail.com>
2024-11-16 14:40:40 +0300

    modified:   src/js/cart.js #	modified:   src/js/product.js #

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,13 +1,19 @@
 import { getLocalStorage } from './utils.mjs';
 
 function renderCartContents() {
-    const cartItems = getLocalStorage('so-cart');
+  const cartItems = getLocalStorage('so-cart') || [];
+  const productList = document.querySelector('.product-list');
+
+  if (cartItems.length === 0) {
+    productList.innerHTML = "<p>Your cart is empty.</p>";
+  } else {
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-    document.querySelector('.product-list').innerHTML = htmlItems.join('');
+    productList.innerHTML = htmlItems.join('');
+  }
 }
 
 function cartItemTemplate(item) {
-    const newItem = `<li class='cart-card divider'>
+  const newItem = `<li class='cart-card divider'>
   <a href='#' class='cart-card__image'>
     <img
       src='${item.Image}'
@@ -22,7 +28,7 @@ function cartItemTemplate(item) {
   <p class='cart-card__price'>$${item.FinalPrice}</p>
 </li>`;
 
-    return newItem;
+  return newItem;
 }
 
 renderCartContents();
--- src/js/product.js
+++ src/js/product.js
@@ -4,14 +4,14 @@ import ProductData from './ProductData.mjs';
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-    const data = getLocalStorage('so-cart');
-    if (data) {
-        data.push(product);
-        setLocalStorage('so-cart', data);
-    } else {
-        setLocalStorage('so-cart', [product]);
+    let data = getLocalStorage('so-cart') || [];
+    if (!Array.isArray(data)) {
+        data = [];
     }
+    data.push(product);
+    setLocalStorage('so-cart', data);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
     const product = await dataSource.findProductById(e.target.dataset.id);
