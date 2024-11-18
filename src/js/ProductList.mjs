9dbe8d76de4a319bea68bfa6d18f98ead42c137e
tiffanyvoorhees2500 <126093924+tiffanyvoorhees2500@users.noreply.github.com>
2024-11-17 21:53:36 -0700

    Trello Product Listing Discount
    Completed task: Product Listing Discount

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,16 +1,21 @@
-import {renderListWithTemplate} from './utils.mjs';
+import { renderListWithTemplate } from './utils.mjs';
 
 // Create a template for displaying all available products
 const productCardTemplate = (product) => {
+    const discountedMessage =
+        product.FinalPrice < product.SuggestedRetailPrice
+            ? `<del>$${parseFloat(product.SuggestedRetailPrice).toFixed(2)}</del><br>`
+            : '';
+
     return `<li class="product-card">
             <a href="product_pages/?product=${product.Id}">
                 <img src="${product.Image}" alt="Image of ${product.NameWithoutBrand}"/>
                 <h3 class="card__brand">${product.Brand.Name}</h3>
                 <h2 class="card__name">${product.NameWithoutBrand}</h2>
-                <p class="product-card__price">${product.FinalPrice}</p>
+                <p class="product-card__price">${discountedMessage}$${product.FinalPrice}</p>
             </a>
         </li>`;
-}
+};
 
 // Create a productListing class
 export default class ProductListing {
@@ -20,12 +25,12 @@ export default class ProductListing {
     }
 
     async init() {
-       const list = await this.dataSource.getData();
-       list.splice(4, list.length);
-       this.renderList(list);
+        const list = await this.dataSource.getData();
+        list.splice(4, list.length);
+        this.renderList(list);
     }
 
-    renderList (list) {
+    renderList(list) {
         renderListWithTemplate(productCardTemplate, this.listElement, list);
     }
 }
