44f4a9feff2764b4d2458ad731450e0f2a8c7038
tiffanyvoorhees2500 <126093924+tiffanyvoorhees2500@users.noreply.github.com>
2024-11-20 11:05:41 -0700

    team activity (minus stretch activity)

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,25 +1,25 @@
 // wrapper for querySelector...returns matching element
 export function qs(selector, parent = document) {
-  return parent.querySelector(selector);
+    return parent.querySelector(selector);
 }
 // or a more concise version if you are into that sort of thing:
 // export const qs = (selector, parent = document) => parent.querySelector(selector);
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
+    return JSON.parse(localStorage.getItem(key));
 }
 // save data to local storage
 export function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
+    localStorage.setItem(key, JSON.stringify(data));
 }
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
-  qs(selector).addEventListener('touchend', (event) => {
-    event.preventDefault();
-    callback();
-  });
-  qs(selector).addEventListener('click', callback);
+    qs(selector).addEventListener('touchend', (event) => {
+        event.preventDefault();
+        callback();
+    });
+    qs(selector).addEventListener('click', callback);
 }
 
 // Create a function to get URL Parameters
@@ -27,7 +27,7 @@ export const getParam = function (param) {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     return urlParams.get(param);
-}
+};
 
 export const renderListWithTemplate = function (templateFn, parentElement, list, position = 'afterbegin', clear = false) {
     const HTMLProdDisplay = list.map(templateFn);
@@ -35,4 +35,34 @@ export const renderListWithTemplate = function (templateFn, parentElement, list,
     if (clear) {
         parentElement.innerHTML = '';
     }
+};
+
+export const renderWithTemplate = function (
+    templateFn,
+    parentElement,
+    data,
+    callback
+) {
+    parentElement.insertAdjacentHTML('afterbegin', templateFn);
+    if (callback) {
+        callback(data);
+    }
+};
+
+export const loadHeaderFooter = async function () {
+    const headerElem = document.getElementById('site-header');
+    const footerElem = document.getElementById('site-footer');
+
+    const headerHtml = await loadTemplate('../partials/header.html');
+    const footerHtml = await loadTemplate('../partials/footer.html');
+
+    renderWithTemplate(headerHtml, headerElem);
+    renderWithTemplate(footerHtml, footerElem);
+};
+
+async function loadTemplate(path) {
+    const html = await fetch(path);
+    const htmlString = await html.text();
+
+    return htmlString;
 }
